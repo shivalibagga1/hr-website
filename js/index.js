402ac1eba37ac98a078153987fb743766a5c500b
TRANTORCHD\shivali.bagga <shivali.bagga@chd.trantorinc.com>
2017-10-05 17:44:40 +0530

    mobile view fixed

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 	$('.collapse-menu').on("click",function(e){
         e.preventDefault();
-		$('#nav a').toggle();
+		$('#nav a:not(:first-child)').toggle();
 	});
     
-	$('.ui.dropdown').dropdown();
+	// $('.ui.dropdown').dropdown();
 });
