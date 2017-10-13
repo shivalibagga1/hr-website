9070efc0efec133ab21ea120aa1de193be3f44b5
TRANTORCHD\shivali.bagga <shivali.bagga@chd.trantorinc.com>
2017-10-13 14:47:44 +0530

    Changes for other functions

--- js/index.js
+++ js/index.js
@@ -6,3 +6,19 @@ $(document).ready(function(){
     
 	// $('.ui.dropdown').dropdown();
 });
+
+
+$(window).scroll(function () {
+    var topOfWindow = $(window).scrollTop(),
+        bottomOfWindow = topOfWindow + $(window).height();
+
+    $('.others').each(function () {
+        var imagePos = $(this).offset().top;
+
+        if(imagePos <= bottomOfWindow && imagePos >= topOfWindow){
+            $(this).addClass('visible');
+        }else{
+            $(this).removeClass('visible');
+        }
+    });
+});
